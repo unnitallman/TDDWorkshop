7ff77a02c574e013f5cc0b658c40a7d455c57f20
unnitallman <unni.tallman@gmail.com>
2011-07-23 12:48:59 +0530

    defined context in tests

--- geometry/spec/geometry/rectangle_spec.rb
+++ geometry/spec/geometry/rectangle_spec.rb
@@ -2,25 +2,28 @@ require 'spec_helper'
 
 module Geometry
   describe Rectangle do
-    it "should have perimeter of 30 for 5x10" do
-      r = Rectangle.new(5,10)
-      r.perimeter.should eq(30)
+    context("the perimeter") do
+      it "is of 30 for 5x10" do
+        r = Rectangle.new(5,10)
+        r.perimeter.should eq(30)
+      end
+
+      it "is of 10 for 2x3" do
+        r = Rectangle.new(2,3)
+        r.perimeter.should eq(10)
+      end
     end
-    
-    it "should have perimeter of 10 for 2x3" do
-      r = Rectangle.new(2,3)
-      r.perimeter.should eq(10)
-    end
-    
-    it "should have area of 50 for 5x10" do
-      r = Rectangle.new(5,10)
-      r.area.should eq(50)
-    end
-    
-     it "should have area of 24 for 8x3" do
+
+    context("the area") do
+      it "is 50 for 5x10" do
+        r = Rectangle.new(5,10)
+        r.area.should eq(50)
+      end
+
+      it "is 24 for 8x3" do
         r = Rectangle.new(8,3)
         r.area.should eq(24)
       end
-    
+    end
   end
 end
